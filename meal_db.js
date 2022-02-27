d04523ffd64191916f91b87dd7cec122cf3367e0
Habibur137 <infernohabib@gmail.com>
2022-02-27 16:22:46 +0600

    single meal details added

--- meal_db.js
+++ meal_db.js
@@ -60,10 +60,35 @@ const displayMealsBySearch = meals => {
             <div class="card-body">
             <h5 class="card-title">${meal.strMeal}</h5>
             <p class="card-text">${meal.strInstructions.slice(0, 200)}</p>
-            <button onclick="loadSingleMeal('single meal')" class='btn btn-success'>Meal Detail</button>
+            <button onclick="loadSingleMeal('${meal.idMeal}')" class='btn btn-success'>Meal Detail</button>
             </div>
         </div>
         `
         mealContainer.append(div)
     })
+}
+
+// single meal detail 
+const loadSingleMeal = (singleMeal) => {
+    console.log(singleMeal)
+    fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${singleMeal}`)
+    .then(res => res.json())
+    .then(data => displaySingleMeal(data.meals[0]))
+}
+
+// display single meal 
+const displaySingleMeal = (singleMeal) => {
+    const mealWrap = document.getElementById('meal-wrap')
+    mealWrap.innerHTML = ''
+    mealWrap.style.display = 'block'
+    const div = document.createElement('div')
+    div.innerHTML = `
+        <img src="${singleMeal.strMealThumb}" class="card-img-top" alt="...">
+        <div class="card-body">
+        <p class="card-text">${singleMeal.idMeal}</p>
+        <h5 class="card-title">${singleMeal.strMeal}</h5>
+        <p class="card-text">${singleMeal.strInstructions.slice(0, 200)}</p>
+        </div>
+    `
+    mealWrap.append(div)
 }
